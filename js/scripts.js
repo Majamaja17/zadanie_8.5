ac384c12a6fb04be217adbc08dd5d435d8afee87
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-12 11:08:41 +0200

    Change js, css, html files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-var a = prompt("5");
-var h = prompt("10");
+var name = prompt('Enter your name and in the next 3 steps you will count the area of the triangle');
+var a = prompt("width of a: 5");
+var h = prompt("width of h: 10");
 var triangleArea = prompt("a*h/2");
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);

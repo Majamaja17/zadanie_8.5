fc3d04db386e063be7778d403135d878fa0bff71
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-14 00:19:28 +0200

    Change js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 var a = prompt("Podaj a");
 var h = prompt("Podaj h");
-var triangleArea = (a*h/2);
+var triangleArea = (a * h / 2);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
